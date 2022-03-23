385fc964fa59c3a485b0fae6cac45a386d490755
andravinicius14@gmail.com <andravinicius14@gmail.com>
2022-03-23 13:27:52 -0300

    teste

---
+++ main.js
@@ -0,0 +1,13 @@
+function somar() {
+  var peso = document.querySelector('input#peso')
+  
+  var p = Number(peso.value)
+   var proteina = p * 2.5;
+   var agua = p * 45;
+   prot = document.querySelector('span#prot')
+   wat = document.querySelector('span#wat')
+   
+   prot.innerHTML = proteina
+   wat.innerHTML = wat
+}
+
