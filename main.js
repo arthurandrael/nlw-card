377b4f31a4fa8707593303b4ca3707f9cc509889
andravinicius14@gmail.com <andravinicius14@gmail.com>
2022-05-07 18:24:42 -0300

    nlw-card

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
-function somar() {
-  var peso = document.querySelector('input#peso')
-  
-  var p = Number(peso.value)
-   var proteina = p * 2.5;
-   var agua = p * 45;
-   prot = document.querySelector('span#prot')
-   wat = document.querySelector('span#wat')
-   
-   prot.innerHTML = proteina
-   wat.innerHTML = wat
+//stage 3
+ const links = {
+   github: "jakeliny",
+   youtube: "jakelinygracielly",
+   facebook: "maykbrito",
+   twitter: "jakelinytec",
+   instagram: "jakeliny.gracielly"
+ }
+
+
+ function changeLinks() {
+   // userName.textContent = 'Marcelo Rocha' //Tem o mesmo efeito do DOM com o uso do getElement
+   for (let li of socialLinks.children) {
+     const social = li.getAttribute('class')
+    
+     li.children[0].href = `https://${social}.com/${links[social]}`
+   }
+ }
+
+ changeLinks()
+
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/user/${links.github}`
 }
 
+fetch(url)
+.then(response => response.json())
+.then(data => {
+  userName.textContent = data.name
+  userBio.textContent = data.bio
+  userLink.href = data.html_url
+  userImage.src = data.avatar_url
+  userLogin = data.login
+  })
